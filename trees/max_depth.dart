fe1c9ca62cef9a6ecb386fc6cc303bdf9daa4ce7
Tanh Nguyen <nvtanh.dev@gmail.com>
2023-04-03 23:53:18 +0700

    :deciduous_tree: TREE max depth

--- trees/max_depth.dart
+++ trees/max_depth.dart
@@ -1,5 +1,7 @@
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/555/
 
+import 'dart:math';
+
 import 'tree_node.dart';
 
 void main(List<String> args) {
@@ -10,24 +12,9 @@ void main(List<String> args) {
 
 class _Solution {
   int maxDepth(TreeNode? root) {
-    // if (root == null) {
-    //   return 0;
-    // }
-    // return 1 + max(maxDepth(root.left), maxDepth(root.right));
-
-    if (root == null) return 0;
-
-    int depth = 0;
-
-    TreeNode? current = root;
-    while (current != null) {
-      depth++;
-      if ((current.left?.val ?? -1) > (current.right?.val ?? -1)) {
-        current = current.left;
-      } else {
-        current = current.right;
-      }
+    if (root == null) {
+      return 0;
     }
-    return depth;
+    return 1 + max(maxDepth(root.left), maxDepth(root.right));
   }
 }
