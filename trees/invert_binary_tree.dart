42cc3e3319473627bca2bb4efa58d51589dad087
Tanh Nguyen <nvtanh.dev@gmail.com>
2023-04-10 22:39:49 +0700

    :deciduous_tree: TREE invert binary tree

---
+++ trees/invert_binary_tree.dart
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/invert-binary-tree/
+
+import 'tree_node.dart';
+
+void main(List<String> args) {
+  // [4,2,7,1,3,6,9]
+  final root = TreeNode(
+    4,
+    TreeNode(
+      2,
+      TreeNode(1, null, null),
+      TreeNode(3, null, null),
+    ),
+    TreeNode(
+      7,
+      TreeNode(6, null, null),
+      TreeNode(9, null, null),
+    ),
+  );
+
+  final rs = _Solution().invertTree(root);
+  print(rs);
+}
+
+class _Solution {
+  TreeNode? invertTree(TreeNode? root) {
+    if (root == null) return root;
+    final temp = invertTree(root.left);
+    root.left = invertTree(root.right);
+    root.right = temp;
+
+    return root;
+  }
+}
