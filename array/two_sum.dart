ff7913c8fd4bb2a60a998196b404979a45292cae
Tanh Nguyen <nvtanh.dev@gmail.com>
2023-04-27 00:05:25 +0700

    :up:  2sum II with 2 pointers solution

--- array/two_sum.dart
+++ array/two_sum.dart
@@ -21,4 +21,23 @@ class Solution {
 
     return List.empty();
   }
+
+  List<int> twoSum2(List<int> nums, int target) {
+    // sorted array
+    // 2 pointers
+
+    var l = 0, r = nums.length - 1;
+    while (l < r) {
+      final sum = nums[l] + nums[r];
+      if (sum == target) {
+        return [l + 1, r + 1];
+      } else if (sum < target) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+
+    return [];
+  }
 }
