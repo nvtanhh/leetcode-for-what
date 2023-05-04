8519d0fab82cc33eefebedf3bf797b2964d07357
Tanh Nguyen <nvtanh.dev@gmail.com>
2023-05-04 21:15:30 +0700

    :rocket:  two sum with hash table solution

--- array/two_sum.dart
+++ array/two_sum.dart
@@ -11,15 +11,27 @@ void main(List<String> args) {
 
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
+    // for (int i = 0; i < nums.length; i++) {
+    //   for (int j = i + 1; j < nums.length; j++) {
+    //     if (nums[i] + nums[j] == target) {
+    //       return [i, j];
+    //     }
+    //   }
+    // }
+
+    // return [];
+
+    final map = <int, int>{};
     for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        if (nums[i] + nums[j] == target) {
-          return [i, j];
-        }
+      final sub = target - nums[i];
+      if (map.containsKey(sub)) {
+        return [map[sub]!, i];
       }
+
+      map[nums[i]] = i;
     }
 
-    return List.empty();
+    return [];
   }
 
   List<int> twoSum2(List<int> nums, int target) {
